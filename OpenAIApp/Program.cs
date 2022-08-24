c532d86500a9db486ad98c72cd3906f595a06d46
Karol <kaajooj@gmail.com>
2022-08-24 14:36:51 +0200

    Init UserSecrets and use it

--- OpenAIApp/Program.cs
+++ OpenAIApp/Program.cs
@@ -2,20 +2,23 @@
 
 using System.Globalization;
 using System.Net.Http.Headers;
+using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 
 Console.WriteLine("Ask a Question:");
 var question = Console.ReadLine();
 
+IConfiguration _configuration = new ConfigurationBuilder()
+    .AddUserSecrets("d9a5cd4e-7c74-46da-a5d5-7321a7d4ae95")
+    .Build();
 
 //call the open ai
-var answer = callOpenAI(250, question, "text-davinci-002", 0.7, 1, 0, 0);
+var answer = callOpenAI(250, question, "text-davinci-002", 0.7, 1, 0, 0, _configuration);
 Console.WriteLine(answer);
 
-static string? callOpenAI(int tokens, string input, string engine, double temperature, int topP, int frequencyPenalty, int presencePenalty)
+static string? callOpenAI(int tokens, string input, string engine, double temperature, int topP, int frequencyPenalty, int presencePenalty, IConfiguration _configuration)
 {
-
-    var openAiKey = "API_KEY";
+    var openAiKey = _configuration["API_KEY"];
 
     var apiCall = "https://api.openai.com/v1/engines/" + engine + "/completions";
 
